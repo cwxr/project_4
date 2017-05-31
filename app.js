05f127b13166809ea9654a33e446acec548cb6b4
robin chua <robinchua@gmail.com>
2017-05-31 16:47:29 +0800

    workable codes

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ app.use('/home', transaction)
 
 require('./routes/user')(app)
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on port 3000')
 })
